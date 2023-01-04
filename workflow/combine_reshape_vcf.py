66a3ed21208144b242df127d76f3907ea7b7b57b
ajtock <35459530+ajtock@users.noreply.github.com>
2023-01-04 17:28:16 +0000

    combine and reshape VCFs

--- workflow/combine_reshape_vcf.py
+++ workflow/combine_reshape_vcf.py
@@ -89,35 +89,38 @@ def cat_vcf_files(indir, info_cols):
             vcf[field] = vcf["INFO"].apply(lambda x: x.get(field, None))
             vcf[field] = vcf[field].astype(dtype)
 
-    # Convert CSQ into a list of subfields
+    # Convert CSQ into a list in which each element is one annotation containing multiple subfields
     vcf["CSQ_ANN_LIST"] = vcf["CSQ"].str.split(",")
+    # Convert each CSQ_ANN_LIST element into a list of subfields
     vcf["CSQ_LIST"] = vcf["CSQ_ANN_LIST"] \
         .apply(lambda x: [y.split("|") for y in x])
-    vcf["Consequence"] = vcf["CSQ_LIST"] \
+    # Extract given subfields from CSQ_LIST as separate columns
+    vcf["consequence"] = vcf["CSQ_LIST"] \
         .apply(lambda x: "__".join(list(dict.fromkeys(list(filter(None, [y[1] for y in x]))).keys())))
-    vcf["IMPACT"] = vcf["CSQ_LIST"] \
+    vcf["impact"] = vcf["CSQ_LIST"] \
         .apply(lambda x: "__".join(list(dict.fromkeys(list(filter(None, [y[2] for y in x]))).keys())))
-    vcf["SYMBOL"] = vcf["CSQ_LIST"] \
+    vcf["gene_symbol"] = vcf["CSQ_LIST"] \
         .apply(lambda x: "__".join(list(dict.fromkeys(list(filter(None, [y[3] for y in x]))).keys())))
-    vcf["Gene"] = vcf["CSQ_LIST"] \
+    vcf["gene_ID"] = vcf["CSQ_LIST"] \
         .apply(lambda x: "__".join(list(dict.fromkeys(list(filter(None, [y[4] for y in x]))).keys())))
-    vcf["Feature_type"] = vcf["CSQ_LIST"] \
+    vcf["feature_type"] = vcf["CSQ_LIST"] \
         .apply(lambda x: "__".join(list(dict.fromkeys(list(filter(None, [y[5] for y in x]))).keys())))
-    vcf["Feature"] = vcf["CSQ_LIST"] \
+    vcf["feature_ID"] = vcf["CSQ_LIST"] \
         .apply(lambda x: "__".join(list(dict.fromkeys(list(filter(None, [y[6] for y in x]))).keys())))
-    vcf["BIOTYPE"] = vcf["CSQ_LIST"] \
+    vcf["biotype"] = vcf["CSQ_LIST"] \
         .apply(lambda x: "__".join(list(dict.fromkeys(list(filter(None, [y[7] for y in x]))).keys())))
 
     # Convert GT into list and add columns containing list elements
     vcf["GT"] = vcf["GT"].str.split(":")
-    vcf["GENOTYPE"] = [x[0] for x in vcf["GT"]]
-    vcf["REF_DEPTH"] = [int(x[1].split(",")[0]) for x in vcf["GT"]]
-    vcf["ALT_DEPTH"] = [int(x[1].split(",")[1]) for x in vcf["GT"]]
-    vcf["VARIANT_DEPTH"] = [int(x[5]) for x in vcf["GT"]]
-    vcf["TOTAL_DEPTH"] = [int(x[3]) for x in vcf["GT"]]
-    vcf["TOTAL_DEPTH_LESS_NO_CALLS"] = vcf["ALT_DEPTH"] + vcf["REF_DEPTH"]
-    vcf["NO_CALLS"] = vcf["TOTAL_DEPTH"] - vcf["TOTAL_DEPTH_LESS_NO_CALLS"]
-    vcf["vaf"] = vcf["ALT_DEPTH"] / (vcf["ALT_DEPTH"] + vcf["REF_DEPTH"])
+    vcf["genotype"] = [x[0] for x in vcf["GT"]]
+    vcf["genotype2"] = ["hom" if x == "1/1" else "het" if x in ["0/1", "1/0"] else "NaN" for x in vcf["genotype"]]
+    vcf["ref_depth"] = [int(x[1].split(",")[0]) for x in vcf["GT"]]
+    vcf["alt_depth"] = [int(x[1].split(",")[1]) for x in vcf["GT"]]
+    vcf["variant_depth"] = [int(x[5]) for x in vcf["GT"]]
+    vcf["total_depth"] = [int(x[3]) for x in vcf["GT"]]
+    vcf["total_depth_less_no_calls"] = vcf["alt_depth"] + vcf["ref_depth"]
+    vcf["no_calls"] = vcf["total_depth"] - vcf["total_depth_less_no_calls"]
+    vcf["vaf"] = vcf["alt_depth"] / (vcf["alt_depth"] + vcf["ref_depth"])
 
     # Remove unnecessary columns
     vcf.drop(columns=["CSQ_ANN_LIST","CSQ_LIST"], inplace=True)
@@ -129,37 +132,10 @@ def cat_vcf_files(indir, info_cols):
     return vcf
 
 
-# Reproduce Canonical SPDI
-# seq_id_map = {
-#     'ABL1': 'NC_000009.12', 'AKT1': 'NC_000014.9', 'ALK': 'NC_000002.12',
-#     'APC': 'NC_000005.10', 'ATM': 'NC_000011.10', 'BRAF': 'NC_000007.14',
-#     'CDH1': 'NC_000016.10', 'CDKN2A': 'NC_000009.12', 'CSF1R': 'NC_000005.10',
-#     'CTNNB1': 'NC_000003.12', 'EGFR': 'NC_000007.14', 'ERBB2': 'NC_000017.11',
-#     'ERBB4': 'NC_000002.12', 'EZH2': 'NC_000007.14', 'FBXW7': 'NC_000004.12',
-#     'FGFR1': 'NC_000008.11', 'FGFR2': 'NC_000010.11', 'FGFR3': 'NC_000004.12',
-#     'FLT3': 'NC_000013.11', 'GNA11': 'NC_000019.10', 'GNAQ': 'NC_000009.12',
-#     'GNAS': 'NC_000020.11', 'HNF1A': 'NC_000012.12', #'HRAS': '',
-#     'IDH1': 'NC_000002.12', 'IDH2': 'NC_000015.10', 'JAK2': 'NC_000009.12',
-#     'JAK3': 'NC_000019.10', 'KDR': 'NC_000004.12', 'KIT': 'NC_000004.12',
-#     'KRAS': 'NC_000012.12', 'MET': 'NC_000007.14', 'MLH1': 'NC_000003.12',
-#     'MPL': 'NC_000001.11', 'NOTCH1': 'NC_000009.12', 'NPM1': 'NC_000005.10',
-#     'NRAS': 'NC_000001.11', 'PDGFRA': 'NC_000004.12', 'PIK3CA': 'NC_000003.12',
-#     'PTEN': 'NC_000010.11', 'PTPN11': 'NC_000012.12', 'RB1': 'NC_000013.11',
-#     'RET': 'NC_000010.11', 'SMAD4': 'NC_000018.10', 'SMARCB1': 'NC_000022.11',
-#     'SMO': 'NC_000007.14', 'SRC': 'NC_000020.11', 'STK11': 'NC_000019.10',
-#     'TP53': 'NC_000017.11', 'VHL': 'NC_000003.12'}
-# sequence_id = seq_id_map[gene]
-#     gene_data['canonical_id'] = (sequence_id + ':'
-#                                  + gene_data.start.astype(str) + ':'
-#                                  + gene_data.ref + ':' + gene_data.alt)
-# https://www.ncbi.nlm.nih.gov/variation/notation/
-# Control?: EGI-02-006-189
-
-
-def merge_mut_mani(vcf_DF, mani_csv):
+def merge_vcf_mani(vcf_DF, mani_csv):
     """
     Extend concatenated VCF DataFrame with additional information from
-    sample manifest CSV, including run, "Subject ID", "cat" (categorical response variable;
+    sample manifest CSV, including "Subject ID", "run_no", "cat" (categorical response variable;
     "CRC+", "Polyps+" or "Control"), and "Mean On-Target Duplex Depth" to be used for
     filtering (>= 1000).
     Output filtered VAF values as matrices.
@@ -174,42 +150,22 @@ def merge_mut_mani(vcf_DF, mani_csv):
                         how="inner", left_on="SAMPLE", right_on="Library Name")
     merge_DF["locus"] = merge_DF["CHROM"].astype(str) + "_" + \
                         merge_DF["POS"].astype(str) + "_" + \
-                        merge_DF["END"].astype(str)
+                        merge_DF["END"].astype(str) + "_" + \
+                        merge_DF["gene_symbol"].astype(str)
     merge_DF_dp = merge_DF.loc[
         (merge_DF["Mean On-Target Duplex Depth"] >= 1000) & \
-        (merge_DF["ALT_DEPTH"] >= 3) & \
-        (merge_DF["FILTER"] == "PASS")
+        (merge_DF["alt_depth"] >= 3) & \
+        (merge_DF["gene_symbol"].str.contains("|".join(gene_list))) & \
+        (~merge_DF["FILTER"].str.contains("|".join(["NM8.0","Q10","q22.5"])))
     ]
-    #
-    file_list = glob.glob(cvdir + "/*.txt", recursive=True)
-    cv_DF_concat = pd.DataFrame()
-    for file_path in file_list:
-        cv_DF = pd.read_csv(file_path, sep="\t")
-        cv_DF_concat = pd.concat(objs=[cv_DF_concat, cv_DF],
-                                 axis=0,
-                                 ignore_index=True)
-    #
-    variant = [re.sub(".+:", "", x) for x in list(cv_DF_concat["Name"])]
-    variant_dna = [re.sub(" .+", "", x) for x in variant]
-    variant_dna2 = [re.sub(" \\(p\\..+\\)", "", x) for x in variant]
-    featureID = [re.sub("\\(.+", "", x) for x in list(cv_DF_concat["Name"])]
-    featureID = [re.sub(":.+", "", x) for x in featureID]
-    clinical_significance = [re.sub("\\(Last reviewed:.+", "", x) \
-                             for x in list(cv_DF_concat["Clinical significance (Last reviewed)"])]
-    cv_DF_concat["variant_dna"] = variant_dna
-    cv_DF_concat["variant_dna2"] = variant_dna2
-    cv_DF_concat["featureID"] = featureID
-    cv_DF_concat["clinical_significance"] = clinical_significance
-    cv_DF_concat_pathogenic = cv_DF_concat[cv_DF_concat["clinical_significance"].str.contains("pathogenic", case=False)]
-    cv_DF_concat_pathogenic_not_conflicting = cv_DF_concat_pathogenic[
-        -cv_DF_concat_pathogenic["clinical_significance"].str.contains("Conflicting", case=False)
+
+    # Get "pathogenic" variants based on VEP-derived ClinVar clinical significance annotations
+    merge_DF_dp_patho = merge_DF_dp.loc[
+        (merge_DF_dp["CSQ"].str.contains("pathogenic", case=False)) & \
+        (~merge_DF_dp["CSQ"].str.contains("Conflicting", case=False))
     ]
-    #
-    merge_DF_dp_patho = merge_DF_dp[merge_DF_dp["TopEffectGeneName"].isin(list(cv_DF_concat_pathogenic_not_conflicting["Gene(s)"]))]
-    # not_LOW
-    merge_DF_dp_patho_CRC_not_LOW = merge_DF_dp_patho.loc[
-        (merge_DF_dp_patho["cat"] == "CRC+") & \
-        (merge_DF_dp_patho["TopEffectImpact"] != "LOW")
+    merge_DF_dp_patho_CRC = merge_DF_dp_patho.loc[
+        (merge_DF_dp_patho["cat"] == "CRC+")
     ]
     merge_DF_dp_patho_Control = merge_DF_dp_patho.loc[
         (merge_DF_dp_patho["cat"] == "Control")
@@ -217,63 +173,40 @@ def merge_mut_mani(vcf_DF, mani_csv):
     merge_DF_dp_patho_Polyps = merge_DF_dp_patho.loc[
         (merge_DF_dp_patho["cat"] == "Polyps+")
     ]
-    #
-    vaf_DF_dp_patho_CRC_not_LOW = merge_DF_dp_patho_CRC_not_LOW.pivot_table(index=["locus", "contig", "start", "end", "TopEffectGeneName", "TopEffectFeatureId"],
-                                                                            columns=["Sample ID", "Subject ID", "cat", "run"],
-                                                                            values="vaf",
-                                                                            fill_value="NaN")
-    vaf_DF_dp_patho_Control = merge_DF_dp_patho_Control.pivot_table(index=["locus", "contig", "start", "end", "TopEffectGeneName", "TopEffectFeatureId"],
-                                                                    columns=["Sample ID", "Subject ID", "cat", "run"],
+
+    # Reshape as matrices of VAF values
+    vaf_DF_dp_patho_CRC = merge_DF_dp_patho_CRC.pivot_table(index=["locus", "CHROM", "POS", "END", "ALT", "gene_symbol", "gene_ID"],
+                                                            columns=["Sample ID", "Subject ID", "cat", "run_no"],
+                                                            values="vaf",
+                                                            fill_value="NaN")
+    vaf_DF_dp_patho_Control = merge_DF_dp_patho_Control.pivot_table(index=["locus", "CHROM", "POS", "END", "ALT", "gene_symbol", "gene_ID"],
+                                                                    columns=["Sample ID", "Subject ID", "cat", "run_no"],
                                                                     values="vaf",
                                                                     fill_value="NaN")
-    vaf_DF_dp_patho_Polyps = merge_DF_dp_patho_Polyps.pivot_table(index=["locus", "contig", "start", "end", "TopEffectGeneName", "TopEffectFeatureId"],
-                                                                  columns=["Sample ID", "Subject ID", "cat", "run"],
+    vaf_DF_dp_patho_Polyps = merge_DF_dp_patho_Polyps.pivot_table(index=["locus", "CHROM", "POS", "END", "ALT", "gene_symbol", "gene_ID"],
+                                                                  columns=["Sample ID", "Subject ID", "cat", "run_no"],
                                                                   values="vaf",
                                                                   fill_value="NaN")
-    #
-    vaf_DF_dp_patho_CRC_not_LOW_merge_Control = pd.merge(left=vaf_DF_dp_patho_CRC_not_LOW,
-                                                         right=vaf_DF_dp_patho_Control,
-                                                         how="left",
-                                                         on=["locus", "contig", "start", "end", "TopEffectGeneName", "TopEffectFeatureId"])
-    vaf_DF_dp_patho_CRC_not_LOW_merge_Control_Polyps = pd.merge(left=vaf_DF_dp_patho_CRC_not_LOW_merge_Control,
-                                                                right=vaf_DF_dp_patho_Polyps,
-                                                                how="left",
-                                                                on=["locus", "contig", "start", "end", "TopEffectGeneName", "TopEffectFeatureId"])
+    # Merge VAF matrices
+    vaf_DF_dp_patho_CRC_merge_Control = pd.merge(left=vaf_DF_dp_patho_CRC,
+                                                 right=vaf_DF_dp_patho_Control,
+                                                 how="left",
+                                                 on=["locus", "CHROM", "POS", "END", "ALT", "gene_symbol", "gene_ID"])
+    vaf_DF_dp_patho_CRC_merge_Control_Polyps = pd.merge(left=vaf_DF_dp_patho_CRC_merge_Control,
+                                                        right=vaf_DF_dp_patho_Polyps,
+                                                        how="left",
+                                                        on=["locus", "CHROM", "POS", "END", "ALT", "gene_symbol", "gene_ID"])
 
-    vaf_DF_dp_patho_CRC_not_LOW_merge_Control.to_csv("vaf_DF_dp_patho_CRC_not_LOW_merge_Control.tsv",
-                                                     na_rep="NaN", sep="\t", header=True, index=True)
-    vaf_DF_dp_patho_CRC_not_LOW_merge_Control_Polyps.to_csv("vaf_DF_dp_patho_CRC_not_LOW_merge_Control_Polyps.tsv",
-                                                            na_rep="NaN", sep="\t", header=True, index=True)
-    #
-    # HIGH
-    merge_DF_dp_patho_CRC_HIGH = merge_DF_dp_patho.loc[
-        (merge_DF_dp_patho["cat"] == "CRC+") & \
-        (merge_DF_dp_patho["TopEffectImpact"] == "HIGH")
-    ]
-    #
-    vaf_DF_dp_patho_CRC_HIGH = merge_DF_dp_patho_CRC_HIGH.pivot_table(index=["locus", "contig", "start", "end", "TopEffectGeneName", "TopEffectFeatureId"],
-                                                                      columns=["Sample ID", "Subject ID", "cat", "run"],
-                                                                      values="vaf",
-                                                                      fill_value="NaN")
-    #
-    vaf_DF_dp_patho_CRC_HIGH_merge_Control = pd.merge(left=vaf_DF_dp_patho_CRC_HIGH,
-                                                      right=vaf_DF_dp_patho_Control,
-                                                      how="left",
-                                                      on=["locus", "contig", "start", "end", "TopEffectGeneName", "TopEffectFeatureId"])
-    vaf_DF_dp_patho_CRC_HIGH_merge_Control_Polyps = pd.merge(left=vaf_DF_dp_patho_CRC_HIGH_merge_Control,
-                                                             right=vaf_DF_dp_patho_Polyps,
-                                                             how="left",
-                                                             on=["locus", "contig", "start", "end", "TopEffectGeneName", "TopEffectFeatureId"])
-    #
-    vaf_DF_dp_patho_CRC_HIGH_merge_Control.to_csv("vaf_DF_dp_patho_CRC_HIGH_merge_Control.tsv",
-                                                  na_rep="NaN", sep="\t", header=True, index=True)
-    vaf_DF_dp_patho_CRC_HIGH_merge_Control_Polyps.to_csv("vaf_DF_dp_patho_CRC_HIGH_merge_Control_Polyps.tsv",
-                                                         na_rep="NaN", sep="\t", header=True, index=True)
-
+    vaf_DF_dp_patho_CRC_merge_Control.to_csv("results/vaf_DF_dp_patho_CRC_not_LOW_merge_Control.tsv",
+                                             na_rep="NaN", sep="\t", header=True, index=True)
+    vaf_DF_dp_patho_CRC_merge_Control_Polyps.to_csv("results/vaf_DF_dp_patho_CRC_merge_Control_Polyps.tsv",
+                                                    na_rep="NaN", sep="\t", header=True, index=True)
 
 
 def main():
-    # Concatenate VCF files
+    # Concatenate VCF files and convert into DataFrame with
+    # additional columns corresponding to subfields of
+    # INFO, CSQ (VEP-generated), and GT fields
     cat_DF = cat_vcf_files(indir=parser.inDir,
                            info_cols={
                                       "END":"int",
@@ -291,12 +224,11 @@ def main():
                                       "CSQ":"str"
                                      })
 
-    #
+
     # Merge MUT and sample manifest tables, and write filtered MUT-derived
     # tables of VAFs as matrices
-    merge_mut_mani(mut_DF=cat_DF,
-                   mani_csv=parser.inDir + "/" + parser.sampleManifest,
-                   cvdir=os.path.join(parser.inDir, parser.cvDir))
+    merge_vcf_mani(vcf_DF=cat_DF,
+                   mani_csv=parser.sampleManifest)
 
 
 if __name__ == "__main__":
